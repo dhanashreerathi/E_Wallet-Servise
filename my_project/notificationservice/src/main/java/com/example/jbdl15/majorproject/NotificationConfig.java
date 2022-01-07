945d42271da0b296813f7bcc48d8d7925e58b869
dhanashreerathi <96377042+dhanashreerathi@users.noreply.github.com>
2022-01-07 20:38:07 +0530

    Add files via upload

---
+++ my_project/notificationservice/src/main/java/com/example/jbdl15/majorproject/NotificationConfig.java
@@ -0,0 +1,67 @@
+package com.example.jbdl15.majorproject;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class NotificationConfig {
+
+    @Bean
+    Properties getKafkaProps(){
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+
+        return properties;
+    }
+
+    @Bean
+    ConsumerFactory<String, String> getConsumerFactory(){
+        return new DefaultKafkaConsumerFactory(getKafkaProps());
+    }
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<String, String> concurrentKafkaListenerContainerFactory(){
+        ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory = new ConcurrentKafkaListenerContainerFactory();
+        kafkaListenerContainerFactory.setConsumerFactory(getConsumerFactory());
+        return kafkaListenerContainerFactory;
+    }
+
+    @Bean
+    ObjectMapper getObjectMapper(){
+        return new ObjectMapper();
+    }
+
+    @Bean
+    JavaMailSender getJavaMailSender(){
+
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setHost("smtp.gmail.com");
+        javaMailSender.setPort(587);
+        javaMailSender.setUsername("gfg.ewallet@gmail.com");
+        javaMailSender.setPassword("gfg123@JBDL13");
+
+        Properties properties = javaMailSender.getJavaMailProperties();
+        properties.put("mail.smtp.starttls.enable",true);
+        properties.put("mail.debug", true);
+
+        return javaMailSender;
+    }
+
+    @Bean
+    SimpleMailMessage getSimpleMailMsg(){
+        return new SimpleMailMessage();
+    }
+}
