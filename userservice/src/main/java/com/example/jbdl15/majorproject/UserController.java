b77b45cc6ae0a1189dfa13ed8983eb8a22fca75e
dhanashreerathi <96377042+dhanashreerathi@users.noreply.github.com>
2022-01-07 20:34:10 +0530

    Add files via upload

---
+++ userservice/src/main/java/com/example/jbdl15/majorproject/UserController.java
@@ -0,0 +1,19 @@
+package com.example.jbdl15.majorproject;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserController {
+    @Autowired
+    UserService userService;
+    @PostMapping("/user")
+    public void createUser(@RequestBody User user) throws JsonProcessingException{
+        userService.createUser(user);
+    }
+    @GetMapping("/user/{userId}")
+    public User getUser(@PathVariable("userId") String userId){
+        return userService.getUser(userId);
+    }
+}
